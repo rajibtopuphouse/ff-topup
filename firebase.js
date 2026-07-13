7300e451d458db66275db89c44369dd40bac63e1
rajibtopuphouse <rstopuphouse@gmail.com>
2026-07-13 22:55:01 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBdaid7-zwzrnPRGwt0qAVbduWm0spx3zw",
+  authDomain: "rajib-top-up-house-49a2b.firebaseapp.com",
+  projectId: "rajib-top-up-house-49a2b",
+  storageBucket: "rajib-top-up-house-49a2b.firebasestorage.app",
+  messagingSenderId: "530922776563",
+  appId: "1:530922776563:web:14c175ce6c779a940a9486"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
