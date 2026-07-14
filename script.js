98a9f487cba7aff63205917e08ee38834dd3a7af
rajibtopuphouse <rstopuphouse@gmail.com>
2026-07-14 17:55:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function placeOrder() {
+
   const uid = document.getElementById("uid").value;
   const name = document.getElementById("name").value;
   const mobile = document.getElementById("mobile").value;
@@ -9,7 +10,18 @@ function placeOrder() {
     return;
   }
 
-  const message =
+  // Firestore-এ অর্ডার সেভ
+  db.collection("orders").add({
+    uid: uid,
+    name: name,
+    mobile: mobile,
+    package: pack,
+    status: "Pending",
+    time: firebase.firestore.FieldValue.serverTimestamp()
+  })
+  .then(function () {
+
+    const message =
 `🎮 Rajib Top-Up House
 
 👤 Name: ${name}
@@ -17,13 +29,22 @@ function placeOrder() {
 📱 Mobile: ${mobile}
 💎 Package: ${pack}`;
 
-  const whatsapp =
-    "https://wa.me/8801788655205?text=" +
-    encodeURIComponent(message);
+    const whatsapp =
+      "https://wa.me/8801788655205?text=" +
+      encodeURIComponent(message);
 
-  window.open(whatsapp, "_blank");
+    window.open(whatsapp, "_blank");
+
+    alert("Order Submitted Successfully");
+
+  })
+  .catch(function (error) {
+    alert(error.message);
+  });
+
 }
 
+// Slider
 const images = [
   "1783852642908.jpg",
   "1783852699516.jpg",
